e6eb2b5e00184327f6bcb9560b465815a75cd115
Santiago Suarez <santiago@Santiagos-MacBook-Pro.local>
2024-12-30 23:18:27 -0500

    Se agregan imágenes para diferentes idiomas

--- src/data/templates.ts
+++ src/data/templates.ts
@@ -1,28 +1,43 @@
 export interface Template {
   id: number;
-  imageUrl: string;
+  imageUrl: {
+    en: string;
+    es: string;
+  };
   demoUrl: string;
 }
 
 export const templates: Template[] = [
   {
     id: 1,
-    imageUrl: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/1._Desktop_Sample_r3kasf.png",
+    imageUrl: {
+      en: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/1._Desktop_Sample_r3kasf.png",
+      es: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/1._Desktop_Sample_es_r3kasf.png"
+    },
     demoUrl: "https://template4.menunow.xyz"
   },
   {
     id: 2,
-    imageUrl: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/3._Desktop_Sample_j1vmvr.png",
+    imageUrl: {
+      en: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/3._Desktop_Sample_j1vmvr.png",
+      es: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/3._Desktop_Sample_es_j1vmvr.png"
+    },
     demoUrl: "https://template3.menunow.xyz"
   },
   {
     id: 3,
-    imageUrl: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/2._Desktop_Sample_w9jg36.png",
+    imageUrl: {
+      en: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/2._Desktop_Sample_w9jg36.png",
+      es: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/2._Desktop_Sample_es_w9jg36.png"
+    },
     demoUrl: "https://template2.menunow.xyz"
   },
   {
     id: 4,
-    imageUrl: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/4._Desktop_Sample_qyad8w.png",
+    imageUrl: {
+      en: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/4._Desktop_Sample_qyad8w.png",
+      es: "https://res.cloudinary.com/du5i6ansa/image/upload/v1733981949/4._Desktop_Sample_es_qyad8w.png"
+    },
     demoUrl: "https://template1.menunow.xyz"
   }
 ];
